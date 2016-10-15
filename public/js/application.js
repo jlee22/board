87759698b87262e2c33c298c602b75af05e19e42
Jeffrey Lee <email": null>
2016-10-15 15:30:39 -0700

    add js logic to hide and show edit user form and button

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $('#edit-user-button').on('click',function() {
+    event.preventDefault();
+    $('#edit-user-button').addClass('hidden');
+    $('#edit-user-string').removeClass('hidden');
+    $('#edit-user-form').removeClass('hidden');
+  });
+  $('#edit-user-form').on('submit',function() {
+    event.preventDefault();
+    $('#edit-user-button').removeClass('hidden');
+    $('#edit-user-string').addClass('hidden');
+    $('#edit-user-form').addClass('hidden');
+  });
 });
