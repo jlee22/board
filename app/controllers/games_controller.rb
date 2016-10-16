6cf244951879d6ef7584ca052f3bc6489e9cc0cc
Jeffrey Lee <email": null>
2016-10-15 18:53:15 -0700

    able to do double query call and extract game description

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,5 +4,18 @@ get '/games/:id/new' do
 end
 
 post '/games' do
+  @game_name =  params[:name]
+  mod_game_name = @game_name.gsub(" ", "%20")
+  game1 = HTTParty.get("http://www.boardgamegeek.com/xmlapi/search?search=#{mod_game_name}&exact=1")
+  game_array1 = game1.first
+  game_id = game_array1.last["boardgame"].first["objectid"]
+  game2 = HTTParty.get("http://boardgamegeek.com/xmlapi2/thing?id=#{game_id}")
 
+
+
+  @game_description = game2.first[1]["item"]["description"].gsub("(from the back of the box:)","").gsub("&#10;","").gsub("&quot;","")
+
+  erb :index
 end
+
+# Dominion id: 36218
