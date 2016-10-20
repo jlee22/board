98ab83115c39e6e287bd4a5d199572c9279d4883
Jeffrey Lee <email": null>
2016-10-20 11:19:21 -0700

    add message button

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -11,19 +11,19 @@ post '/messages' do
   @contact = User.find(params[:id])
   @source = Resource.find(params[:api_id])
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
-  resp = Net::HTTP.get_response(URI.parse(@source.link))
-
   account_sid = ENV['TWILLIO_SID']
   auth_token = ENV['TWILLIO_AUTH_TOKEN']
 
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  resp = Net::HTTP.get_response(URI.parse(@source.link))
 
   if params[:custom_text].length > 0
   @text = params[:custom_text]
+  end
   @client.account.messages.create({
     :from => '+1 408-872-9724',
-    :to => @contact.phonenumber,
+    :to => "+1 + #{@contact.phone_number}",
     :body => @text,
-  end
+  })
 
 end
