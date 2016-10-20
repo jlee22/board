29f731fb080015b4506680a77d14d8af2657270a
Jeffrey Lee <email": null>
2016-10-20 14:35:59 -0700

    setting up new message function by adding routes and partial logic to loop through players

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -69,5 +69,39 @@ delete '/games/:id' do
 end
 
 
+get '/games/:id/messages/new' do
+  @game = Game.find(params[:id])
+  @contacts = @game.players
+  # @source = Resource.all
+  erb :'messages/new'
+end
+
+
+post '/games/:id/messages' do
+  @game = Game.find(params[:id])
+  @contacts = @game.players # array of player objects
+  # @source = Resource.find(params[:api_id])
+
+  account_sid = ENV['TWILLIO_SID']
+  auth_token = ENV['TWILLIO_AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  resp = Net::HTTP.get_response(URI.parse(@source.link))
+
+  if params[:custom_text].length > 0
+  @text = params[:custom_text]
+  else
+  erb :'messages/new'
+  end
+  @contacts.each do |player|
+  @client.account.messages.create({
+    :from => "+1 408-872-9724",
+    :to => "+1 + #{player.phone_number}",
+    :body => @text,
+  })
+  end
+
+end
+
 # Dominion id: 36218
 
