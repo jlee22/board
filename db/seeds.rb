5fb1d492433a3d23eedad76d00ac0cf10cff9233
Jeffrey Lee <email": null>
2016-10-21 12:20:05 -0700

    fix my seed file type

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ end
 
 # User.create(username: 'j', email: 'j@j', address: 'j jt.', phone_number: '2027310710', password: 'j')
 
-Game.create(name: 'Setters of Catan', description: "Strategic card drafting board game that challenges that players to think dynamically", minplayers:2, maxplayers: 4, minage: 13, host: User.first)
+Game.create(name: 'Settlers of Catan', description: "Strategic card drafting board game that challenges that players to think dynamically", minplayers:2, maxplayers: 4, minage: 13, host: User.first)
 
 Game.create(name: 'Ticket to Ride', description: "Players complete the complete the best tracks across the country", minplayers:2, maxplayers: 4, minage: 6, host: User.last)
